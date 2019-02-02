57f9e614b8e7b512979ad5b6cf04f222f00f79c9
ilyafuu <xXmadara1Xx@gmail.com>
2019-02-02 15:16:50 +0300

    o4dz

--- Scripts/Main.gd
+++ Scripts/Main.gd
@@ -1,16 +1,24 @@
 extends Node
 
+onready var clicklg : Array = [$HUDclickLE/Panel/VBoxContainer/TButtonLanguages,
+		$HUDclickLE/Panel/VBoxContainer/TButtonLanguages2]
 
 func _ready() -> void:
-	
-	
 	$ScreenColor/AnimationPlayer.play('screen_st')
+	$HUDclickLE/AnimationPlayer.play('mhstopst')
+	
+	for i in clicklg:
+		i.connect('indexlg',self,'fclicklg')
 
 
 
 
 func anim_screen_st_time_end():
-	pass
+	$HUDclickLE/AnimationPlayer.play('mhstart')
+
+
+func fclicklg(indexlg):
+	print('click ',indexlg)
 
 
 func new_game():
