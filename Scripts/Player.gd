ac50d3ff8d3429e218ba69090992ad31f62a8a0a
ilyafuu <xXmadara1Xx@gmail.com>
2019-02-03 22:35:35 +0300

    h9xg

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,11 +1,11 @@
 extends KinematicBody2D
 
 export(int) var walk_speed = 200
+#export(int) var run_speed = 300
+
 
 var speed : float = 0
-
 var motion : Vector2
-var iswalk : bool = false
 
 func _ready():
 	$AnimationTree['parameters/run/blend_amount'] = 0
@@ -25,14 +25,32 @@ func _physics_process(delta):
 
 func _input(event):
 	if event.is_action_pressed("ui_left"):
+		charspeed(0)
+	elif event.is_action_released("ui_left"):
+		charspeed(2)
+	elif event.is_action_pressed('ui_right'):
+		charspeed(1)
+	elif event.is_action_released("ui_right"):
+		charspeed(2)
+	
+	if event.is_action_pressed("ui_pickup"):
+		pass
+	if event.is_action_pressed("ui_run"):
+		pass
+	elif event.is_action_released("ui_run"):
+		pass
+	if event.is_action_pressed("ui_flashlight"):
+		pass
+
+
+func charspeed(indexspeed):
+	if indexspeed == 0:
 		speed = -walk_speed
 		$Sprite.flip_h = true
-	elif event.is_action_released("ui_left"):
-		speed = 0
-	elif event.is_action_pressed('ui_right'):
+	elif indexspeed == 1:
 		speed = walk_speed
 		$Sprite.flip_h = false
-	elif event.is_action_released("ui_right"):
+	else:
 		speed = 0
 
 
