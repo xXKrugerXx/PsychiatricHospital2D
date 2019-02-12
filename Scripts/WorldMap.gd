ba895aed53db8f4c44d977f4d507bce2fe387d9d
ilyafuu <xXmadara1Xx@gmail.com>
2019-02-12 12:09:55 +0300

    kh4k

--- Scripts/WorldMap.gd
+++ Scripts/WorldMap.gd
@@ -1,14 +1,14 @@
 extends Node
 
 var arrstairs : Array = [0,0,0]
+var arrsignalstairs : Array = []
 
 func _ready():
 	for s in $signalstairs.get_children():
 		s.connect('signalstairs',self,'fsignalstairs')
-	arrstairs[1] = -1
-	
-	print(arrstairs)
-
+	for st in $stairs.get_children():
+		for c in st.get_children():
+			arrsignalstairs.append(c)
 
 
 
@@ -17,4 +17,10 @@ func _ready():
 func fsignalstairs(index):
 	match index:
 		0:
-			print('asd')
+			if arrstairs[0] == 0:
+				arrstairs[0] +=1
+				$stairs/stairs00R.set_collision_layer_bit(1,true)
+			else:
+				arrstairs[0] -= 1
+				$stairs/stairs00R.set_collision_layer_bit(1,false)
+				
