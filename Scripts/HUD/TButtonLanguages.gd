2327597ad5db74f9c198f4e6580d2c9eadffb617
Demonevil <xXmadara1Xx@gmail.com>
2019-02-26 15:46:18 +0300

    ms0r

--- Scripts/HUD/TButtonLanguages.gd
+++ Scripts/HUD/TButtonLanguages.gd
@@ -1,11 +1,8 @@
-extends TextureButton
+extends Button
 
 signal indexlg
 
 export(int) var clickindex = 0
 
-func fclick():
-	emit_signal('indexlg', clickindex)
-
 func _on_TButtonLanguages_pressed():
-	fclick()
+	emit_signal('indexlg', clickindex)
